6d115f139e70f6a35afb8a66c5ae8c98e3fba54f
Quentin Gonzalez <q.gonz95@gmail.com>
2022-11-01 13:53:25 -0400

    ok

---
+++ app.js
@@ -0,0 +1,7 @@
+$(()=>{
+// const $homeHead = $('<h3>').addClass('state').text('Junior Software Engineer with a background in Personal Training and Competitive boxing.')
+// $('body').append($homeHead)
+
+
+
+})

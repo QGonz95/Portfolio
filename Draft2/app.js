534df8d3a512d22a5c386b12a72c7c8f6b4ea494
Quentin Gonzalez <q.gonz95@gmail.com>
2022-11-03 16:05:18 -0400

    moving forawrd!!!

---
+++ Draft2/app.js
@@ -0,0 +1,29 @@
+$(() => {
+    /// IMAGE CAROUSEL FUNCTION PREVIOUS AND NEXT BUTTON \\\
+      let currentImgIndex = 0;
+      let numOfImages = $('.imgs').children().length - 1
+  
+      $('.next').on('click', () =>{
+        $('.imgs').children().eq(currentImgIndex).css('display', 'none')
+  
+        if(currentImgIndex < numOfImages) {
+          currentImgIndex ++
+         } else {
+          currentImgIndex = 0
+         }   
+         
+          $('.imgs').children().eq(currentImgIndex).css('display', 'block')       
+      })
+  
+      $('.prev').on('click', () => {
+        $('.imgs').children().eq(currentImgIndex).css('display', 'none')
+  
+        if(currentImgIndex > 0) {
+          currentImgIndex --
+        } else {
+          currentImgIndex = numOfImages
+        }   
+          
+          $('.imgs').children().eq(currentImgIndex).css('display', 'block')       
+      })
+  })

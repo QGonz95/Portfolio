86dfeaa068c249703ce33711e95c87e4e37968c0
Quentin Gonzalez <q.gonz95@gmail.com>
2022-11-04 14:01:13 -0400

    im happy

--- Draft2/app.js
+++ Draft2/app.js
@@ -26,6 +26,16 @@ $(() => {
           
           $('.imgs').children().eq(currentImgIndex).css('display', 'block')       
       })
+    //   ME MYSELF AND I click me
+const $aboutMe = $('#About')
+const $i = $('<div>').addClass('myself')
+const $img1 = $('<img src= /png/punch.jpeg />').addClass('img1')
+const $p1 = $('<p>').addClass('box').text("'March 10 2017, Quentin Gonzalez throws punch of the night, defeats Matthew Mahoney during Daily News Golden Gloves' It feels like yesterday that Daily News headline was published. So much is put on the line to dream as big as I try to. Time is ticking away and I fear oppurtunities fade faster than the body.")
+const about = () => {
+    $i.append($img1)
+    $i.append($p1)
+    $('#About').append($i)
+}
     //   OPEN AND CLOSE BUTTONS FOR FOOTER MODALS 
     const $openBtn = $('#openModal');
 const $modal = $('#modal');
@@ -48,7 +58,7 @@ const closeModal2 = () => {
     $modal2.css('display', 'none')
 }
 
-
+$aboutMe.on('click', about)
 $openBtn.on('click', openModal)
 $closeBtn.on('click', closeModal)
 $openBtn2.on('click', openModal2)
